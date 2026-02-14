daf36fce22bbcf27f91329a07bd3be7a461ba26a
kysonhensley <hensleykyson@gmail.com>
2026-02-13 23:23:47 -0700

    create method that when called will add new user messages to existing log of messages

--- AccumulateMessagesFunction/Program.cs
+++ AccumulateMessagesFunction/Program.cs
@@ -1,17 +1,30 @@
-﻿namespace AccumulateMessagesFunction
+﻿using System.Xml.Schema;
+
+namespace AccumulateMessagesFunction
 {
     internal class Program
     {
+        /*
         // TODO []: Define a function that takes a string message and appends it to a accumulating result
         // TODO []: Initialize an empty string to hold accumulated messages
         // TODO []: Create a loop to allow user to enter multiple messages
-        // TODO []:Add logic to stop the loop when user chooses to stop
+        // TODO []: Add logic to stop the loop when user chooses to stop
         // TODO []: Display the accumulated messages at the end
         // TODO []: Wait for user to press Enter before closing the console window
+        */
         
         static void Main(string[] args)
         {
+            // define new variable to hold accumulated messages
+            string accumulatedMessages = "";
+
             Console.WriteLine("Hello, World!"); 
+        }
+
+        static string AccumulateMessages(string message, string accumulatedMessages)
+        {
+            // TODO []: Append the new message to the accumulated messages
+            return accumulatedMessages + message + "\n";
         }
     }
 }
