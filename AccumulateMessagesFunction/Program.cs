63c73d86e4fbd194b938deb78731618d17d4bc3e
kysonhensley <hensleykyson@gmail.com>
2026-02-13 23:38:02 -0700

    Create strong variable that holds user input message, create while loop to continue program until user decides to quit, call accumaltemessages method to accumulate user input message

--- AccumulateMessagesFunction/Program.cs
+++ AccumulateMessagesFunction/Program.cs
@@ -5,11 +5,11 @@ namespace AccumulateMessagesFunction
     internal class Program
     {
         /*
-        // TODO []: Define a function that takes a string message and appends it to a accumulating result
-        // TODO []: Initialize an empty string to hold accumulated messages
-        // TODO []: Create a loop to allow user to enter multiple messages
-        // TODO []: Add logic to stop the loop when user chooses to stop
-        // TODO []: Display the accumulated messages at the end
+        // TODO [x]: Define a function that takes a string message and appends it to a accumulating result
+        // TODO [x]: Initialize an empty string to hold accumulated messages
+        // TODO [x]: Create a loop to allow user to enter multiple messages
+        // TODO [x]: Add logic to stop the loop when user chooses to stop
+        // TODO [x]: Display the accumulated messages at the end
         // TODO []: Wait for user to press Enter before closing the console window
         */
         
@@ -17,13 +17,34 @@ namespace AccumulateMessagesFunction
         {
             // define new variable to hold accumulated messages
             string accumulatedMessages = "";
+            string message = "";
+            // boolean variable to determine of user has decided to quit
+            bool userQuit = true;
 
-            Console.WriteLine("Hello, World!"); 
+            // while loop to allow user to enter multiple messages until they choose to quit
+            while (userQuit)
+            {
+                Console.WriteLine("Enter a message to accumulate or type 'quit' to stop and show accumulated messages:");
+                // read user input
+                message = Console.ReadLine();
+                // check if user wants to quit
+                if (message.ToLower() == "quit")
+                {
+                    userQuit = false;
+                }
+                // if user hasnt quit, accumulate the message
+                else
+                {
+                    accumulatedMessages = AccumulateMessages(message, accumulatedMessages);
+                }
+            }
+            Console.Clear();
+            Console.WriteLine($"You entered the following messages: \n{accumulatedMessages}"); 
+            Console.ReadLine();
         }
 
         static string AccumulateMessages(string message, string accumulatedMessages)
         {
-            // TODO []: Append the new message to the accumulated messages
             return accumulatedMessages + message + "\n";
         }
     }
